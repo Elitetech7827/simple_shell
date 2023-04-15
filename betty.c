ed412a4886fdb1562e2b057af54e99d96cf0cb5b
Hi_Ren <onyerene1999@yahoo.com>
2023-04-15 04:02:56 -0700

    simple shell

---
+++ betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A simple code that passes the Betty checks
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("This is Task 0 of Simple shell project\n");
+
+	Return(0);
+}
